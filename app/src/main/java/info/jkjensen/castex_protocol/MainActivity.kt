7b9e2f37928758e0bf353a512a758c71cf04c75d
Jk Jensen <azjkjensen@gmail.com>
2018-02-16 14:58:52 -0700

    Added screen capture.

--- app/src/main/java/info/jkjensen/castex_protocol/MainActivity.kt
+++ app/src/main/java/info/jkjensen/castex_protocol/MainActivity.kt
@@ -1,103 +1,77 @@
 package info.jkjensen.castex_protocol
 
+import android.Manifest
 import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.content.Context
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.ImageFormat
 import android.graphics.PixelFormat
-import android.os.Environment
-import java.io.File
-import java.io.FileOutputStream
-import java.util.*
+import android.hardware.display.DisplayManager
+import android.media.Image
+import android.media.ImageReader
 import android.net.Uri
+import android.os.Bundle
+import android.provider.Settings
 import android.support.v4.app.ActivityCompat
-import android.support.v4.content.FileProvider
+import android.support.v7.app.AppCompatActivity
+import android.util.DisplayMetrics
 import android.util.Log
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 import android.view.WindowManager
 import android.widget.RelativeLayout
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.experimental.launch
+import org.jetbrains.anko.mediaProjectionManager
+import java.io.File
+import java.io.FileOutputStream
+import java.lang.Thread.sleep
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
-    val TAG ="info.jkjensen.castex"
+
+    companion object {
+        val TAG ="info.jkjensen.castex"
+        val MEDIA_PROJECTION_RESULT_CODE = "mediaprojectionresultcode"
+        val MEDIA_PROJECTION_RESULT_DATA = "mediaprojectionresultdata"
+        private val REQUEST_MEDIA_PROJECTION_CODE = 101
+        private val REQUEST_OVERLAY_CODE = 201
+        private val REQUEST_FILE_CODE = 301
+    }
+
+    var passCount = 0
     var filepath:String? = null
-    var imageFiles:ArrayList<File> = arrayListOf()
+    var images:ArrayList<Bitmap> = arrayListOf()
     var imageFile:File? = null
     val REQUEST_CODE_EXTERNAL_STORAGE_PERMISSION:Int = 2
     var frameNumber:Int = 1
+    var metrics:DisplayMetrics? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         take.setOnClickListener {
-
-            Thread(Runnable {
-                while(true){
-                    frameNumber = frameNumber + 1
-                    Log.d(TAG, "Running frame " +  frameNumber)
-                    takeScreenshot()
-                }
-            }).start()
+            Log.d(TAG, "Clicked")
         }
         open.setOnClickListener { openScreenshot()}
         open.isEnabled = false
 
-//        ActivityCompat.requestPermissions(this,
-//                arrayOf(WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE),
-//                REQUEST_CODE_EXTERNAL_STORAGE_PERMISSION)
+        ActivityCompat.requestPermissions(this,
+                arrayOf(WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE),
+                REQUEST_FILE_CODE)
 
-//        val layout: RelativeLayout = layoutInflater.inflate(R.layout.bg_surface_view, this) as RelativeLayout
-        val params = WindowManager.LayoutParams(1,1,
-                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
-                WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH or WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                PixelFormat.TRANSPARENT)
-
-//        val wm = getSystemService(Context.WINDOW_SERVICE) as WindowManager
-//        wm.addView(relative, params)
-        val svf: SurfaceView = surface_view_fake as SurfaceView
-        val sh: SurfaceHolder = svf.holder
-        svf.setZOrderOnTop(true)
-        sh.setFormat(PixelFormat.TRANSPARENT)
-        sh.addCallback(object: SurfaceHolder.Callback {
-            override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
-                // This is where the magic happens
-                var bitmap = Bitmap.createBitmap(svf.width, svf.height, Bitmap.Config.ARGB_8888)
-                var canv = Canvas(bitmap)
-                val now = Date()
-                android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now)
-                filepath = Environment.getExternalStorageDirectory().toString() + "/" + now + ".jpg"
-                svf.draw(canv)
-                imageFile = File(filepath)
-                synchronized(this){
-                    imageFiles.add(imageFile!!)
-                }
-                val outputStream = FileOutputStream(imageFile)
-                val quality = 100
-                bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream)
-                outputStream.close()
-
-            }
-
-            override fun surfaceDestroyed(holder: SurfaceHolder?) {
-            }
-
-            override fun surfaceCreated(holder: SurfaceHolder?) {
-            }
-
-        })
-
-//       Thread(Runnable {
-//           while(true) {
-//               openScreenshot()
-//
-//           }
-//       }).start()
+        if(!Settings.canDrawOverlays(this)) {
+            val overlayIntent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
+            overlayIntent.data = Uri.parse("package:" + packageName)
+            startActivityForResult(overlayIntent, REQUEST_OVERLAY_CODE)
+        } else{
+            startActivityForResult(
+                    mediaProjectionManager.createScreenCaptureIntent(),
+                    REQUEST_MEDIA_PROJECTION_CODE)
+        }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -106,42 +80,143 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun takeScreenshot() {
-//        val now = Date()
-//        android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now)
-//
-//        try {
-//            // image naming and path  to include sd card  appending name you choose for file
-//            filepath = Environment.getExternalStorageDirectory().toString() + "/" + now + ".jpg"
-//
-//            // create bitmap screen capture
-//            val v1 = window.decorView.rootView
-//            v1.isDrawingCacheEnabled = true
-//            val bitmap = Bitmap.createBitmap(v1.drawingCache)
-//            v1.isDrawingCacheEnabled = false
-//
-//            imageFile = File(filepath)
-//            synchronized(this){
-//                imageFiles.add(imageFile!!)
-//            }
-//            val outputStream = FileOutputStream(imageFile)
-//            val quality = 100
-//            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream)
-//            myimageview.setImageBitmap(bitmap)
-//            outputStream.flush()
-//            outputStream.close()
-//        } catch (e: Throwable) {
-//            // Several error may come out with file handling or DOM
-//            e.printStackTrace()
-//        }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_MEDIA_PROJECTION_CODE) {
+            super.onActivityResult(requestCode, resultCode, data)
+            val mediaProjection = mediaProjectionManager.getMediaProjection(resultCode, data)
 
+            metrics = applicationContext.resources.displayMetrics
+
+
+//            val layout: RelativeLayout = layoutInflater.inflate(R.layout.bg_surface_view, null) as RelativeLayout
+//            val params = WindowManager.LayoutParams(1,1,
+//                    WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+//                    WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH or WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+//                    PixelFormat.TRANSPARENT)
+//
+//            val wm = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+//            wm.addView(layout, params)
+//
+//            val svf:SurfaceView = layout.findViewById(R.id.surface_view_fake)
+
+            val imageReader = ImageReader.newInstance(metrics!!.widthPixels, metrics!!.heightPixels, ImageFormat.JPEG, 5);
+            mediaProjection.createVirtualDisplay("test", metrics!!.widthPixels, metrics!!.heightPixels, metrics!!.density.toInt(),
+                    WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH or WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                    imageReader.getSurface(), null, null);
+            var image: Image? = null
+            var bitmap: Bitmap? = null
+            imageReader.setOnImageAvailableListener(object: ImageReader.OnImageAvailableListener {
+
+                override fun onImageAvailable(reader: ImageReader?) {
+                    image = null;
+//                    FileOutputStream fos = null;
+                    bitmap = null;
+
+                    try {
+                        image = imageReader.acquireLatestImage()
+//                        val fos = FileOutputStream(filesDir + "/myscreen.jpg")
+                        val planes = image!!.planes
+                        val buffer = planes[0].buffer.rewind()
+                        bitmap = Bitmap.createBitmap(metrics!!.widthPixels, metrics!!.heightPixels, Bitmap.Config.ARGB_8888)
+//                        bitmap.copyPixelsFromBuffer(buffer)
+//                        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
+                        images.add(bitmap!!)
+
+
+                    } catch (e: Exception) {
+                        e.printStackTrace();
+                    } finally {
+//                        if (fos!=null) {
+//                            try {
+//                                fos.close();
+//                            } catch (IOException ioe) {
+//                                ioe.printStackTrace();
+//                            }
+//                        }
+
+                        if (bitmap != null)
+                            bitmap?.recycle()
+
+                        if (image != null)
+                            image?.close()
+                    }
+
+                }
+            }, null)
+
+
+//            val svf: SurfaceView = surface_view_fake as SurfaceView
+//            val sh: SurfaceHolder = svf.holder
+//            svf.setZOrderOnTop(true)
+////            sh.setFormat(PixelFormat.TRANSPARENT)
+//            val virtualDisplay = mediaProjection.createVirtualDisplay("ScreenCapture",
+//                    metrics!!.widthPixels, metrics!!.heightPixels, metrics!!.densityDpi,
+//                    DisplayManager.VIRTUAL_DISPLAY_FLAG_PRESENTATION,
+//                    sh.surface, null, null)
+//            sh.addCallback(object : SurfaceHolder.Callback {
+//                override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
+//
+//                }
+//
+//                override fun surfaceDestroyed(holder: SurfaceHolder?) {
+//                }
+//
+//                override fun surfaceCreated(holder: SurfaceHolder?) {
+//                }
+//
+//            })
+//            // Surface is ready, start collecting images.
+//            val bitmap = Bitmap.createBitmap(metrics!!.widthPixels, metrics!!.heightPixels, Bitmap.Config.ARGB_8888)
+//            val canv = Canvas(bitmap)
+//
+//            Thread(Runnable {
+//                while (true) {
+//                    sleep(1000)
+//                    passCount++
+//                    // This is where the magic happens
+//                    val bitmap = Bitmap.createBitmap(metrics!!.widthPixels, metrics!!.heightPixels, Bitmap.Config.ARGB_8888)
+//                    val canv = Canvas(bitmap)
+//                    synchronized(this) {
+//                        svf.draw(canv)
+//                        if(passCount == 2){
+//                            Log.d(TAG, "PASSINGGGG")
+//                            bitmap.saveToDateFile()
+//                        }
+//                        Log.d(TAG, "Adding image to array")
+//                        images.add(bitmap)
+//                    }
+//
+//                }
+//            }).start()
+
+            Thread(Runnable {
+                while (true) {
+                    sleep(1000)
+                    openScreenshot()
+
+                }
+            }).start()
+        } else if(requestCode == REQUEST_OVERLAY_CODE){
+            startActivityForResult(
+                    mediaProjectionManager.createScreenCaptureIntent(),
+                    REQUEST_MEDIA_PROJECTION_CODE)
+        }
     }
 
     @Synchronized private fun openScreenshot() {
-        if(imageFiles.isEmpty()){
+        if(images.isEmpty()){
+            Log.d(TAG, "Image array is empty")
             return
         }
-        val uri = Uri.fromFile(imageFiles.removeAt(0))
-//        myimageview.setImageURI(uri)
+//        val uri = Uri.fromFile(images.removeAt(0))
+        runOnUiThread(object: Runnable{
+            override fun run() {
+                if(images.isEmpty()){
+                    return
+                }
+                Log.d(TAG, "Updating imageview")
+                myimageview.setImageBitmap(images.removeAt(0))
+            }
+        })
     }
 }
---
+++ app/src/main/java/info/jkjensen/castex_protocol/Utils.kt
@@ -0,0 +1,27 @@
+package info.jkjensen.castex_protocol
+
+import android.graphics.Bitmap
+import android.os.Environment
+import java.io.File
+import java.io.FileOutputStream
+import java.util.*
+
+/**
+ * Created by jk on 2/16/18.
+ * A set of utility functions to simplify application code.
+ */
+
+/**
+ * Saves the bitmap to a simple JPEG file with the date as the name.
+ */
+fun Bitmap.saveToDateFile(){
+
+    val now = Date()
+    android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now)
+    val filepath = Environment.getExternalStorageDirectory().toString() + "/" + now + ".jpg"
+    val imageFile = File(filepath)
+    val outputStream = FileOutputStream(imageFile)
+    val quality = 100
+    this.compress(Bitmap.CompressFormat.JPEG, quality, outputStream)
+    outputStream.close()
+}
